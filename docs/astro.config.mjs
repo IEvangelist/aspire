dc1d180f600b2f9f4798614896731d326ddf265c
Maddy Montaquila <maleger@microsoft.com>
2025-05-29 17:10:33 -0400

    imported Outfit font to use for headers

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,8 +8,7 @@ import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://IEvangelist.github.io',
-	base: 'aspire',
+	site: 'https://aspire.dev',
 	integrations: [
 		starlight({
 			title: 'Aspire',
@@ -41,7 +40,7 @@ export default defineConfig({
 				{
 					icon: 'x.com',
 					label: 'X',
-					href: 'https://x.com/dotnet'
+					href: 'https://x.com/aspiredotdev'
 				},
 				{
 					icon: 'blueSky',
@@ -70,91 +69,35 @@ export default defineConfig({
 				}
 			],
 			customCss: [
-				'./src/styles/site.css'
+				'./src/styles/site.css',
+				'@fontsource-variable/outfit'
 			],
 			components: {
 				ContentPanel: './src/components/ContentPanel.astro',
 				SocialIcons: './src/components/SocialIcons.astro',
 				Search: './src/components/Search.astro',
-				Footer: './src/components/Footer.astro',
-				MarkdownContent: './src/components/MarkdownContent.astro',
+				Footer: './src/components/Footer.astro'
 			},
 			expressiveCode: {
 				/* TODO: decide which themes we want
 				   https://expressive-code.com/guides/themes/#using-bundled-themes
 				*/
-				themes: ['github-dark-default', 'github-light']
+				themes: ['catppuccin-mocha', 'catppuccin-latte']
 			},
 			sidebar: [
-				{
-					label: 'Home',
-					link: '/'
-				},
 				{
 					label: 'Welcome',
 					items: [
 						{ label: 'Overview', slug: 'get-started/overview' },
 						{ label: 'Prerequisites', slug: 'get-started/prerequisites' },
 						{ label: 'Installation', slug: 'get-started/installation' },
-						{ label: 'First app', slug: 'get-started/first-app', badge: 'Quickstart' }
+						{ label: 'First app', slug: 'get-started/first-app' }
 					],
 				},
 				{
-					label: 'Build',
-					badge: {
-						text: 'Concepts',
-						variant: 'note'
-					},
-					autogenerate: { directory: 'build' },
-				},
-				{
-					label: 'Dashboard',
-					items: [
-						{ label: 'Overview', slug: '' },
-						{
-							label: 'Features', slug: '', badge: {
-								text: 'UX',
-								variant: 'caution'
-							},
-						},
-						{
-							label: 'Standalone', slug: '', badge: {
-								text: 'Container',
-								variant: 'danger'
-							},
-						},
-						{ label: 'Configuration', slug: '' },
-						{ label: 'Browser telemetry', slug: '' },
-					]
-				},
-				{
-					label: 'Integrations',
-					collapsed: true,
-					autogenerate: { directory: 'integrations' },
-				},
-				{
-					label: 'Custom Integrations',
-					collapsed: true,
-					items: [
-						{ label: 'Create hosting integration', slug: '' },
-						{ label: 'Create client integration', slug: '' },
-						{ label: 'Secure integrations', slug: '' },
-					]
-				},
-				{
-					label: 'Deploy',
-					collapsed: true,
-					autogenerate: { directory: 'deploy' },
-				},
-				{
 					label: 'Reference',
-					badge: {
-						text: 'API',
-						variant: 'tip'
-					},
-					collapsed: true,
 					autogenerate: { directory: 'reference' },
-				}
+				},
 			],
 			plugins: [
 				catppuccin(),
@@ -164,11 +107,11 @@ export default defineConfig({
 				}),
 				starlightKbd({
 					types: [
-						{ id: 'mac', label: 'macOS' },
-						{ id: 'windows', label: 'Windows', default: true },
-						{ id: 'linux', label: 'Linux' },
+					  { id: 'mac', label: 'macOS' },
+					  { id: 'windows', label: 'Windows', default: true },
+					  { id: 'linux', label: 'Linux' },
 					],
-				}),
+				  }),
 			],
 		}),
 	],
