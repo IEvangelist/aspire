ead48699df20fb6f1c7f62e4dab01dcc15325332
David Pine <david.pine@microsoft.com>
2025-08-04 23:48:56 -0500

    Add a bit more to the contributors.

--- docs/scripts/update-github-stats.js
+++ docs/scripts/update-github-stats.js
@@ -3,7 +3,8 @@ import fetch from 'node-fetch';
 
 const REPOS = [
     'dotnet/aspire',
-    'dotnet/aspire-samples'
+    'dotnet/aspire-samples',
+    'dotnet/docs-aspire'
 ];
 const OUTPUT_PATH = './src/data/github-stats.json';
 
